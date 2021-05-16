4c3e31987e5925fa62e3dfe15e8cc6eb6c51580a
lixq94 <2247572065@qq.com>
2021-05-16 11:10:53 +0800

    第一次提交

---
+++ src/com/git/TestGitDemo01.java
@@ -0,0 +1,8 @@
+package com.git;
+
+public class TestGitDemo01 {
+    public static void main(String[] args) {
+        System.out.println("这是TestGitDemo01");
+        System.out.println("修改了TestGitDemo01");
+    }
+}
